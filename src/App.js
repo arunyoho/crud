2aab1b20d095fe36959c28842d1042ca777b841b
arunyoho <140370911+arunyoho@users.noreply.github.com>
2024-09-09 13:26:50 +0530

    arun.commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route ,Routes } from 'react-router-dom';
+import Create from './Create';
+import Update from './Update';
+import Read from './Read';
+import Home from './Home';
+
+function App() {
+  return (
+   <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='/create' element={<Create />}></Route>
+    <Route path='/update/:id' element={<Update />}></Route>
+    <Route path='/read/:id' element={<Read />}></Route>
+  </Routes>
+
+   </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  });
+
+  return (
+    <div className=" p-5 mt-5 d-flex flex-column justify-content-center align-items-center bg-light vh-100">
+      <h1>List of Users</h1>
+      <div className="w-75 rounded bg-white boder shadow p-4">
+        <div className="d-flex justify-content-end ">
+          <Link to="/create" className="btn btn-success">
+            Add +
+          </Link>
+        </div>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((d, i) => (
+              <tr key={i}>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.email}</td>
+                <td>{d.phone}</td>
+                <td>
+                  <button className="btn btn-sm btn-info me-2">Read</button>
+                  <button className="btn btn-sm btn-primary me-2">Edit</button>
+                  <button className="btn btn-sm btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
